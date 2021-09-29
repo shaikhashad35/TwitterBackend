43af136747a33049d82298a071b4ab304c640de1
shaikhashad35 <shaikhashad35@gmail.com>
2021-09-29 19:40:57 +0530

    port change

--- src/app.js
+++ src/app.js
@@ -14,37 +14,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-//Authentication middle ware which authenticates the token before calling any api.
-const AuthMiddleware = async (req, res, next) => {
-  //get the token from header
-  let token = req.headers["twi-token"];
-
-  //Token not found
-  if (!token) {
-    return res.status(401).send({ er: "No token found", auth: false });
-  }
-
-  //verify the token
-  jwt.verify(token, process.env.SECRET_KEY, async (err, decode) => {
-    if (err) {
-      return res.status(500).send({ auth: false, er: "Authentication failed" });
-    }
-
-    let result = await UserModel.find({
-      username: decode.username, // get the Usermodel from the decode username
-    });
-
-    //appending the user detail in header request
-    req.user = result[0];
-    next();
-  });
-
-  console.log(req.body);
-};
-
 app.use("/user", user);
 app.use("/tweet", tweet);
 
-app.listen(PORT, function () {
-  console.log(`App listening at http://localhost:${PORT}`);
+app.listen(process.env.PORT || 5000, function () {
+  console.log(`App listening at http://localhost:${process.env.PORT || 5000}`);
 });
